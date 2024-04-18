d12c814bdde9bb322ac88a0bae453724be0b8eb9
yadokani389 <yadokani389@gmail.com>
2024-04-18 20:30:09 +0900

    add extra fonts

--- hosts/desktop/fonts/default.nix
+++ hosts/desktop/fonts/default.nix
@@ -1,11 +1,13 @@
 { config, lib, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ twemoji-color-font ];
+  environment.systemPackages = with pkgs; [ jetbrains-mono ];
   fonts = {
     packages = with pkgs; [
+      noto-fonts
       noto-fonts-cjk
       noto-fonts-emoji
+      noto-fonts-extra
       twemoji-color-font
       fira-code-symbols
       (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" "Noto" ]; })
