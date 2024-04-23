af6fa8f9f40571d24eb4725b5c820bfe28090296
yadokani389 <yadokani389@gmail.com>
2024-04-23 22:17:36 +0900

    Change nix-on-droid Font

--- hosts/bune/nix-on-droid.nix
+++ hosts/bune/nix-on-droid.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, hostname, username, config, ... }: {
+{ inputs, pkgs, hostname, username, config, system, ... }: {
   environment = {
     packages = with pkgs; [ vim ];
     etcBackupExtension = ".bak";
@@ -12,10 +12,16 @@
 
   time.timeZone = "Asia/Tokyo";
 
+  user.shell = "${pkgs.zsh}/bin/zsh";
+
+  terminal.font = "${
+      pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; }
+    }/share/fonts/truetype/NerdFonts/JetBrainsMonoNerdFontMono-Regular.ttf";
+
   home-manager = {
     config = ./home-manager.nix;
     backupFileExtension = "hm-bak";
     useGlobalPkgs = true;
-    extraSpecialArgs = { inherit inputs username; };
+    extraSpecialArgs = { inherit inputs username system; };
   };
 }
