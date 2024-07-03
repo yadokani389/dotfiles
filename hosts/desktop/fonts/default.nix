3d156981aec095b42309d233a9946012e35bd6f9
yadokani389 <yadokani389@gmail.com>
2024-07-03 09:06:23 +0900

    font: set default fonts

--- hosts/desktop/fonts/default.nix
+++ hosts/desktop/fonts/default.nix
@@ -5,12 +5,21 @@
   fonts = {
     packages = with pkgs; [
       noto-fonts
-      noto-fonts-cjk
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
       noto-fonts-emoji
       noto-fonts-extra
       twemoji-color-font
       fira-code-symbols
       (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" "Noto" ]; })
     ];
+    fontconfig = {
+      defaultFonts = {
+        serif = [ "Noto Serif CJK JP" "Noto Color Emoji" ];
+        sansSerif = [ "Noto Sans CJK JP" "Noto Color Emoji" ];
+        monospace = [ "FiraCode Nerd Font" "Noto Color Emoji" ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
   };
 }
