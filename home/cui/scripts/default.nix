05eb3556e5987f40752ef1ab3c7d6ba3dc1cf1a1
yadokani389 <yadokani389@gmail.com>
2024-07-03 09:06:59 +0900

    script: remove unused script

--- home/cui/scripts/default.nix
+++ home/cui/scripts/default.nix
@@ -4,9 +4,6 @@ let
   cava-internal = pkgs.writeShellScriptBin "cava-internal" ''
     cava -p ~/.config/cava/config1 | sed -u 's/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g;'
   '';
-  rofiWindow = pkgs.writeShellScriptBin "rofiWindow" ''
-    rofi -show drun -theme "$HOME/.config/rofi/theme.rasi"
-  '';
   wallpaper_random = pkgs.writeShellScriptBin "wallpaper_random" ''
     if command -v swww >/dev/null 2>&1; then 
 
@@ -40,4 +37,4 @@ let
       echo "swww not found"
     fi
   '';
-in { home.packages = with pkgs; [ cava-internal rofiWindow wallpaper_random ]; }
+in { home.packages = with pkgs; [ cava-internal wallpaper_random ]; }
