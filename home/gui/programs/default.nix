f7934881db46e38c0f6b463eca9e016c559b3a73
yadokani389 <yadokani389@gmail.com>
2024-07-18 16:40:00 +0900

    vscode: remove vscode

--- home/gui/programs/default.nix
+++ home/gui/programs/default.nix
@@ -5,7 +5,6 @@
     ./kitty.nix
     ./fuzzel.nix
     ./waybar.nix
-    ./vscode.nix
     ./hyprlock.nix
     ./wlogout.nix
   ];
--- home/gui/programs/vscode.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }: {
-  programs.vscode = {
-    enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      mgt19937.typst-preview
-      nvarner.typst-lsp
-    ];
-  };
-}
