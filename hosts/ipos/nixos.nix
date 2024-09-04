7c406a0c014198c965ee272c1f7676794495b4f8
yadokani389 <yadokani389@gmail.com>
2024-09-04 15:39:06 +0900

    ipos: change tlp settings

--- hosts/ipos/nixos.nix
+++ hosts/ipos/nixos.nix
@@ -42,7 +42,23 @@
 
   services = {
     thermald.enable = true;
-    tlp.enable = true;
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "performance";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 100;
+
+        STOP_CHARGE_THRESH_BAT0 = 90;
+      };
+    };
   };
 
   programs.hyprland = {
