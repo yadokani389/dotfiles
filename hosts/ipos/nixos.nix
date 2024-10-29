d4390ce07abd19b4ff5292dca0920ea57a8fcb79
yadokani389 <yadokani389@gmail.com>
2024-10-29 18:16:27 +0900

    tlp: remove

--- hosts/ipos/nixos.nix
+++ hosts/ipos/nixos.nix
@@ -45,23 +45,6 @@
 
   services = {
     thermald.enable = true;
-    tlp = {
-      enable = true;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "performance";
-
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 100;
-
-        STOP_CHARGE_THRESH_BAT0 = 90;
-      };
-    };
     logind.lidSwitch = "ignore";
 
     xserver.desktopManager.gnome.enable = true;
