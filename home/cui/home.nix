22d2bcfff097515a5238dd26e0aa54cc78a37ae1
yadokani389 <yadokani389@gmail.com>
2024-11-28 21:14:51 +0900

    sptlrx: add with v1.2.2

--- home/cui/home.nix
+++ home/cui/home.nix
@@ -28,5 +28,16 @@
     cachix
     tdf
     comma
+    (sptlrx.overrideAttrs (old: rec {
+      version = "1.2.2";
+      src = pkgs.fetchFromGitHub {
+        owner = "raitonoberu";
+        repo = "sptlrx";
+        rev = "v${version}";
+        hash = "sha256-b8ALhEjolH0RH+I9HVQeOagPBi2isLNUxqKdj5u2O9s=";
+      };
+      vendorHash = "sha256-pExSQcYjqliZZg/91t52yk6UJ4QCbpToMpONIFUNkwc=";
+      doCheck = false;
+    }))
   ]) ++ [ inputs.nixvim.packages."${pkgs.system}".default ];
 }
