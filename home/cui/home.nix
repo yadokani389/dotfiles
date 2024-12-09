04c1fd651ff5374f4b2173b4aa5b8655828ea26d
yadokani389 <yadokani389@gmail.com>
2024-12-09 19:50:12 +0900

    nix: remove unused packages

--- home/cui/home.nix
+++ home/cui/home.nix
@@ -3,15 +3,10 @@
   imports = [ ./programs ./themes ];
 
   home.packages = with pkgs; [
-    gnumake
     gh
     cava
-    tty-clock
     btop
     playerctl
-    neofetch
-    typstyle
-    tinymist
     tdf
     sptlrx
     cargo-compete
