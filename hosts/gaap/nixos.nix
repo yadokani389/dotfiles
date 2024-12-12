305d26d895e1ed2c7cea5257dbd0b0d45d8485d9
yadokani389 <yadokani389@gmail.com>
2024-12-12 17:05:14 +0900

    gaap: simplify

--- hosts/gaap/nixos.nix
+++ hosts/gaap/nixos.nix
@@ -1,6 +1,4 @@
-{ pkgs, inputs, hostname, username, ... }: {
-  # imports = [ ../nixos.nix ];
-
+{ pkgs, inputs, username, ... }: {
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
     tmp.useTmpfs = true;
@@ -10,7 +8,6 @@
     host.pkgs = import inputs.nixpkgs {
       system = "x86_64-linux";
       config.allowUnfree = true;
-      overlays = [ (import ../../pkgs/default.nix) ];
     };
     memorySize = 1024 * 16;
     cores = 12;
@@ -22,7 +19,7 @@
   users.users."${username}" = {
     isNormalUser = true;
     shell = pkgs.zsh;
-    extraGroups = [ "networkmanager" "wheel" "audio" "video" ];
+    extraGroups = [ "wheel" ];
     hashedPassword =
       "$6$xzVNYSD7yHJuO./x$5fCLN3.ENzMJDWkgegYazIgw/NkWYC2jMSiTDqma84wjEhbYRgeDPcHb.nc55WPD3qpACqGakvM4kXHZihgly0";
   };
@@ -32,28 +29,9 @@
     git.enable = true;
   };
 
-  networking.hostName = hostname;
-
-  nix = {
-    package = pkgs.lix;
-
-    settings = {
-      auto-optimise-store = true;
-      keep-outputs = true;
-      experimental-features = [ "nix-command" "flakes" ];
-      trusted-users = [ "root" "${username}" ];
-
-      substituters = [
-        "https://cache.nixos.org?priority=10"
-        "https://nix-community.cachix.org"
-        "https://yadokani389.cachix.org"
-      ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "yadokani389.cachix.org-1:xHw9jijQFNDKlNprHbQpXX6cVOUO4m/n2lBfx6Bq4jg="
-      ];
-    };
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    trusted-users = [ "root" "${username}" ];
   };
 
   system.stateVersion = "24.05";
