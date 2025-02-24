9343323fb17383ec243d126abdb11a2e66ebecf5
yadokani389 <yadokani389@gmail.com>
2025-02-25 01:26:07 +0900

    vscode: setup

--- home/gui/programs/vscode.nix
+++ home/gui/programs/vscode.nix
@@ -2,17 +2,24 @@
 {
   programs.vscode = {
     enable = true;
-    extensions =
-      (with inputs.nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace; [
-        ms-vsliveshare.vsliveshare
-        rust-lang.rust-analyzer
-        tauri-apps.tauri-vscode
-        vue.volar
-        github.copilot
-        coolcline.coolcline
-      ])
-      ++ [
-        inputs.nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace-release.github.copilot-chat
-      ];
+    profiles.default = {
+      userSettings = {
+        "files.autoSave" = "afterDelay";
+        "files.insertFinalNewline" = true;
+      };
+      extensions =
+        (with inputs.nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace; [
+          ms-vsliveshare.vsliveshare
+          rust-lang.rust-analyzer
+          tauri-apps.tauri-vscode
+          vue.volar
+          github.copilot
+          coolcline.coolcline
+          mkhl.direnv
+        ])
+        ++ [
+          inputs.nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace-release.github.copilot-chat
+        ];
+    };
   };
 }
