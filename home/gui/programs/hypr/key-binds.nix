933c49e4018f4eb16bf20bc100aee5a9f7ceb194
yadokani389 <yadokani389@gmail.com>
2025-06-02 01:13:27 +0900

    hyprland: remove keybind

--- home/gui/programs/hypr/key-binds.nix
+++ home/gui/programs/hypr/key-binds.nix
@@ -77,7 +77,6 @@
       # Move/resize windows with mainMod + LMB/RMB and dragging
       "$mainMod, mouse:272, movewindow"
       "$mainMod, mouse:273, resizewindow"
-      "ALT, mouse:272, resizewindow"
     ];
   };
 }
