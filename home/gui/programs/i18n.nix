c1ede5068860684ea52e05eb09537241c2a00e74
yadokani389 <yadokani389@gmail.com>
2026-03-16 03:10:18 +0900

    fcitx5-hazkey: remove

--- home/gui/programs/i18n.nix
+++ home/gui/programs/i18n.nix
@@ -4,29 +4,10 @@
     enable = true;
     type = "fcitx5";
     fcitx5 = {
-      fcitx5-with-addons = pkgs.callPackage (
-        {
-          buildFHSEnv,
-          addons ? [ ],
-        }:
-        buildFHSEnv {
-          name = "fcitx5";
-          targetPkgs = pkgs: [
-            (pkgs.qt6Packages.fcitx5-with-addons.override { inherit addons; })
-          ];
-          runScript = "fcitx5";
-        }
-      ) { };
       waylandFrontend = true;
       addons = with pkgs; [
-        fcitx5-hazkey
         karukan-im
       ];
     };
   };
-
-  home.file.".config/hazkey/environment".text = ''
-    export LIBLLAMA_PATH=${pkgs.hazkey-zenzai}/lib/hazkey/llama/libllama.so
-    export HAZKEY_ZENZAI_MODEL=${pkgs.hazkey-zenzai}/share/hazkey/zenzai.gguf
-  '';
 }
