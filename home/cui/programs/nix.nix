417d4a6670be7bfec56c9401efdee3334a78643e
yadokani389 <yadokani389@gmail.com>
2026-04-27 00:00:05 +0900

    nix: add cuda cache

--- home/cui/programs/nix.nix
+++ home/cui/programs/nix.nix
@@ -5,12 +5,14 @@
       "https://nix-community.cachix.org"
       "https://yadokani389.cachix.org"
       "https://oxalica.cachix.org"
+      "https://cache.nixos-cuda.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "yadokani389.cachix.org-1:xHw9jijQFNDKlNprHbQpXX6cVOUO4m/n2lBfx6Bq4jg="
       "oxalica.cachix.org-1:h0iRBw6tQD8+51ZvnNEBPbwLR58UD7klauDBWzBdugQ="
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
     ];
   };
 }
