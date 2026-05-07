ba91afc3e25290d3b27a4d40543f98455c53b44a
yadokani389 <yadokani389@gmail.com>
2026-05-07 15:40:58 +0900

    yazi: add some plugins

--- home/cui/programs/yazi.nix
+++ home/cui/programs/yazi.nix
@@ -23,5 +23,37 @@
         }
       ];
     };
+
+    keymap = {
+      mgr.prepend_keymap = [
+        {
+          on = "f";
+          run = "plugin jump-to-char";
+          desc = "Jump to char";
+        }
+        {
+          on = "F";
+          run = "plugin smart-filter";
+          desc = "Smart filter";
+        }
+        {
+          on = [ "<C-d>" ];
+          run = "plugin drag";
+          desc = "Drag Files";
+        }
+      ];
+    };
+
+    plugins = {
+      inherit (pkgs.yaziPlugins)
+        jump-to-char
+        smart-filter
+        drag
+        ;
+    };
   };
+
+  home.packages = with pkgs; [
+    ripdrag
+  ];
 }
